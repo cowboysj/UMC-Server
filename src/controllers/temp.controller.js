9c7d82c187429009f46bbd8f51525e6946695c1f
cowboysj <su10jin11@khu.ac.kr>
2023-11-22 15:50:17 +0900

    에러 핸들링 추가

--- src/controllers/temp.controller.js
+++ src/controllers/temp.controller.js
@@ -1,6 +1,13 @@
 import { status } from "../../config/response.status.js";
 import { getTempData } from "../services/temp.service.js";
 import { response } from "../../config/response.js";
+import { CheckFlag } from "../services/temp.service.js";
+
 export const tempTest = (req, res, next) => {
   res.send(response(status.SUCCESS, getTempData()));
+};
+
+export const tempException = (req, res, next) => {
+  console.log("/temp/exception/" + req.params.flag); //flag 파라미터 가져오기
+  return res.send(response(status.SUCCESS, CheckFlag(req.params.flag)));
 };

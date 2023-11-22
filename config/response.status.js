924cdb30122fa9e534fd5fbc867f78b907ae23cf
cowboysj <su10jin11@khu.ac.kr>
2023-11-22 14:37:48 +0900

    8주차 실습

---
+++ config/response.status.js
@@ -0,0 +1,10 @@
+import { StatusCodes } from "http-status-codes";
+export const status = {
+  // success
+  SUCCESS: {
+    status: 200,
+    isSuccess: true,
+    code: 2000,
+    message: "success!",
+  },
+};
---
+++ src/services/temp.service.js
@@ -0,0 +1,4 @@
+import { tempResponseDTO } from "../dtos/temp.response.dto.js";
+export const getTempData = () => {
+  return tempResponseDTO("This is TEST! >.0");
+};

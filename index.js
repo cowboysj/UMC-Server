09f4ff87bd28c99f1e06eb5267cf8e2901a9ce4f
cowboysj <su10jin11@khu.ac.kr>
2023-11-29 23:40:16 +0900

    :sparkles: Feat : 회원가입 api
     회원가입 api 추가

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import express from "express";
 import { tempRouter } from "./src/routes/temp.route.js";
 import { response } from "./config/response.js";
 import { status } from "./config/response.status.js";
+import { userRouter } from "./src/routes/user.route.js";
+import { specs } from "./config/swagger.config.js";
+import SwaggerUi from "swagger-ui-express";
+
 const app = express();
 const port = 3007;
-
-// router setting
-app.use("/temp", tempRouter);
 
 // error handling
 /* app.use((err, req, res, next) => {
@@ -25,3 +26,11 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+// swagger
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
+
+// router setting
+app.use("/temp", tempRouter);
+app.use("/user", userRouter);
